9bce69a81bd9275a3a3b42c0c8fc8d0617e675ab
PedroViniciusMD <pedrodelgado583@gmail.com>
2026-07-20 14:45:59 -0300

    feat: report infos

--- main.sh
+++ main.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-#config your dir (pwd)
 read -p "Config Your Dir: " dir
 
 if [[ ! -d "$dir" ]]
@@ -8,6 +7,9 @@ then
     echo "Error: The directory '$dir' does not exist."
     exit 1
 fi
+
+start_time=$(date +%s)
+echo ""
 
 img_count=0
 vid_count=0
@@ -57,20 +59,50 @@ do
     fi
 done
 
+shopt -u nocasematch
+
 total_count=$(( img_count+vid_count+music_count+doc_count+slide_count+other_count ))
+end_time=$(date +%s)
+execution_time=$(( end_time - start_time ))
+timestamp=$(date '+%Y%m%d_%H%M%S')
 
 #Report Infos
+echo "=========================================="
+echo "*** Report ($(date '+%Y-%m-%d %H:%M:%S')) ***"
+echo "=========================================="
+echo "Images moved: $img_count" 
+echo "Videos moved: $vid_count" 
+echo "Musics moved: $music_count"
+echo "Docs moved: $doc_count" 
+echo "Slides moved: $slide_count" 
+echo "Other Types moved: $other_count" 
+echo "------------------------------------------"
+echo "Total Files moved: $total_count"
+echo "Time elapsed in seconds: ${execution_time}s"
+echo "=========================================="
+echo ""
+#echo $start_time
+#echo $end_time
+
+
+#Report File
 mkdir -p "$dir/reports"
 report_file="$dir/reports/report.txt"
 
-
-echo "*** Report ($(date '+%Y-%m-%d %H:%M:%S')) ***" > "$report_file"
-echo "Images moved: $img_count" >> "$report_file"
-echo "Videos moved: $vid_count" >> "$report_file"
-echo "Musics moved: $music_count" >> "$report_file"
-echo "Docs moved: $doc_count" >> "$report_file"
-echo "Slides moved: $slide_count" >> "$report_file"
+echo "==========================================" > "$report_file"
+echo "*** RUN LOG: $(date '+%Y-%m-%d %H:%M:%S') ***" >> "$report_file"
+echo "==========================================" >> "$report_file"
+echo "Executed by      : $USER" >> "$report_file"
+echo "Machine          : $HOSTNAME" >> "$report_file"
+echo "Target Directory : $dir" >> "$report_file"
+echo "------------------------------------------" >> "$report_file"
+echo "Images moved     : $img_count" >> "$report_file"
+echo "Videos moved     : $vid_count" >> "$report_file"
+echo "Musics moved     : $music_count" >> "$report_file"
+echo "Docs moved       : $doc_count" >> "$report_file"
+echo "Slides moved     : $slide_count" >> "$report_file"
 echo "Other Types moved: $other_count" >> "$report_file"
-echo "Total Archives moved: $total_count" >> "$report_file"
-
-shopt -u nocasematch
+echo "------------------------------------------" >> "$report_file"
+echo "Total Files      : $total_count" >> "$report_file"
+echo "Execution Time   : ${execution_time}s" >> "$report_file"
+echo "==========================================" >> "$report_file"
